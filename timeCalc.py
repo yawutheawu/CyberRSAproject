34aa3b3c9277df0011f33a668df0e37bb93ec3f5
yawutheawu <mayklprime@gmail.com>
2023-04-13 09:28:27 -0400

    Repl Time Calc update

--- timeCalc.py
+++ timeCalc.py
@@ -1,3 +1,8 @@
 import RSAdemo
 import randomBruteForce
 import Messages as m1
+import time
+
+#https://pynative.com/python-get-execution-time-of-program/
+#after getting the execution time for a few different sized keys,
+#best fit a curve and then store the equation to estimate future times
